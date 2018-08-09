1cf2300590aa7faefb64b28312734251af59128f
mercenery <mercenery@inbox.ru>
2018-08-09 08:36:25 +0300

    add querryes

--- resources/zaprosy.sql
+++ resources/zaprosy.sql
@@ -6,7 +6,7 @@ where SALARY = (select max(SALARY)
                 where e1.DEPARTMENT_ID = e2.DEPARTMENT_ID)
 order by DEPARTMENT_ID;
 
---находим самые высокие зарплаты в отделах
+--находим самые высокие зарплаты в отделах и кто их получает
 select distinct
   DEPARTMENT_ID,
   FIRST_NAME,
@@ -16,6 +16,23 @@ select distinct
     partition by DEPARTMENT_ID ) MAXIMALNIE_SALARY
 from DENKLE.EMPLOYEES
 order by DEPARTMENT_ID;
+
+select max(SALARY)
+from DENKLE.EMPLOYEES
+group by DEPARTMENT_ID;
+
+select
+  EMPLOYEES.DEPARTMENT_ID,
+  max(SALARY)
+from DENKLE.EMPLOYEES
+  join DENKLE.DEPARTMENTS on EMPLOYEES.DEPARTMENT_ID = DEPARTMENTS.DEPARTMENT_ID
+group by EMPLOYEES.DEPARTMENT_ID;
+
+select e1.*
+from DENKLE.EMPLOYEES e1
+where e1.SALARY = (select max(e2.SALARY)
+                   from DENKLE.EMPLOYEES e2
+                   where e1.DEPARTMENT_ID = e2.DEPARTMENT_ID);
 
 --находим сотрудников со второй самой высокой зарплатой с конца
 select *
@@ -30,3 +47,26 @@ where SALARY = (select max(SALARY)
 select *
 from DENKLE.EMPLOYEES e1, DENKLE.EMPLOYEES e2
 where e1.SALARY > e2.SALARY and e1.MANAGER_ID = e2.EMPLOYEE_ID;
+
+--Вывести список ID отделов, количество сотрудников в которых не превышает 3 человек
+select DEPARTMENT_ID
+from DENKLE.EMPLOYEES
+group by DEPARTMENT_ID
+having count(*) < 3;
+
+--Вывести список сотрудников, не имеющих назначенного руководителя, работающего в том-же отделе
+select * -- выбираем список
+from DENKLE.EMPLOYEES e1e -- из таблицы эмплои
+  left join DENKLE.EMPLOYEES e2m  -- объединяем с собой же
+    on e2m.EMPLOYEE_ID =
+       e1e.MANAGER_ID -- где id мэнеджера сотрудника таблицы сотрудников = ID сотрудника из таблицы менеджеров
+where e2m.DEPARTMENT_ID <> e1e.DEPARTMENT_ID; -- там где ID отдела менеджера не равен ID отдела его подчинённого
+
+-- Найти список ID отделов с максимальной суммарной зарплатой сотрудников
+with sum_salary as
+( select department_id, sum(salary) salary
+  from   DENKLE.EMPLOYEES
+  group  by department_id )
+select department_id
+from   sum_salary a
+where  a.salary = ( select max(salary) from sum_salary );
--- src/запросы.sql
+++ src/запросы.sql
@@ -1,125 +1,72 @@
-SELECT
-  manager_id,
-  last_name,
-  salary,
-  SUM(salary)
-  OVER (
-    PARTITION BY manager_id
-    ORDER BY SALARY rows UNBOUNDED PRECEDING ) l_csum
-FROM DENKLE.employees;
-
-select *
-from DENKLE.EMPLOYEES e
-  join DENKLE.DEPARTMENTS d on e.DEPARTMENT_ID = d.DEPARTMENT_ID
-group by SALARY;
-
-select
-  distinct
-  DEPARTMENT_ID,
-  sum(SALARY)
-  over (
-    partition by DEPARTMENT_ID
-    order by DEPARTMENT_ID range unbounded preceding ) as salary_sum_by_departments
-from DENKLE.EMPLOYEES
-order by DEPARTMENT_ID;
-
-select distinct
-  DEPARTMENT_ID,
-  sum(SALARY)
-  over (
-    partition by DEPARTMENT_ID
-    order by DEPARTMENT_ID range unbounded preceding ) as sssss
-from DENKLE.EMPLOYEES
-order by DEPARTMENT_ID;
-
--- функция берёт таблицу из неё выбирает указанные колонки разбивает таблицу по id отдела(OVER) и применяет для каждого
--- RANGE-по id отдела суммирует все салари в одну сумму, если указать ROWS то операция  SUM() будет производиться для
--- каждой отличающейся по зарплате строки и для каждого отдела складывает суммы всех ежемесячных зарплат ячейку.
-select distinct
-  department_id,
-  sum(SALARY)
-  over (
-    partition by DEPARTMENT_ID
-    order by DEPARTMENT_ID range unbounded preceding )
-from DENKLE.EMPLOYEES
-order by DEPARTMENT_ID;
-
---вторая с конца по размеру зарплата
-select max(SALARY) as second_max_desc
-from DENKLE.EMPLOYEES
-where SALARY not in (select max(SALARY)
-                     from DENKLE.EMPLOYEES);
-
---test
-select
-  e.DEPARTMENT_ID,
-  e.EMPLOYEE_ID,
-  e.FIRST_NAME,
-  e.LAST_NAME,
-  e.SALARY,
-  '  CHIEF_WITH_SMALL_SALARY',
-  m.LAST_NAME,
-  m.SALARY,
-  (e.SALARY - m.SALARY) as difference_in_salary
-from DENKLE.EMPLOYEES e, DENKLE.EMPLOYEES m
-where e.DEPARTMENT_ID = m.DEPARTMENT_ID and e.SALARY > m.SALARY
-order by e.DEPARTMENT_ID desc;
-
---Вывести список сотрудников, получающих заработную плату большую чем у непосредственного руководителя
-select
-  e.DEPARTMENT_ID,
-  e.EMPLOYEE_ID,
-  e.FIRST_NAME,
-  e.SALARY,
-  e.MANAGER_ID
-from DENKLE.EMPLOYEES e, DENKLE.EMPLOYEES m
-where e.MANAGER_ID = m.EMPLOYEE_ID and e.SALARY > m.SALARY;
-
---максимальные зарплаты по отделам
-select max(SALARY) max_sal
-from DENKLE.EMPLOYEES e
-  join DENKLE.DEPARTMENTS d on e.DEPARTMENT_ID = d.DEPARTMENT_ID
-group by DEPARTMENT_NAME
-order by max_sal desc;
-
---людей с максимальными зарплатами по отделам
-
-select distinct *
-from DENKLE.EMPLOYEES e1
-where SALARY in (select max(SALARY)
-over (
-  partition by e2.DEPARTMENT_ID
-  order by e1.EMPLOYEE_ID range unbounded preceding )
-                 from DENKLE.EMPLOYEES e2)
-order by DEPARTMENT_ID;
-
+-- находим сотрудников с самой высокой зарплатой в отделах
 select *
 from DENKLE.EMPLOYEES e1
-where SALARY = (select MAX(SALARY)
+where SALARY = (select max(SALARY)
                 from DENKLE.EMPLOYEES e2
                 where e1.DEPARTMENT_ID = e2.DEPARTMENT_ID)
-order by EMPLOYEE_ID;
--- select distinct -- выбрать следующий набор колонок для отображения:
---   DEPARTMENT_ID, -- номер отдела
---   EMPLOYEE_ID, -- номер сотрудника
---   FIRST_NAME, -- имя сотрудника
---   LAST_NAME, -- фамилию сотрудника
---   SALARY -- его зарплату
--- from DENKLE.EMPLOYEES -- из таблицы эмплои
--- where SALARY in (select max(SALARY) -- где зарплата сотрудника в диапазоне максимальной зарплаты
--- over ( -- где диапазон определяется
---   partition by DEPARTMENT_ID) -- номером отдела
---                  from DENKLE.EMPLOYEES) -- таблицы эмплои
--- order by DEPARTMENT_ID desc ; -- отобразить ранжировав по номеру отдела в обратном порядке
---
--- select
---   DEPARTMENT_ID,
---   FIRST_NAME,
---   LAST_NAME,
---   SALARY
--- from DENKLE.EMPLOYEES
--- where SALARY in (select max(SALARY) max_sal
---                  from DENKLE.EMPLOYEES e
---                    join DENKLE.DEPARTMENTS d on e.DEPARTMENT_ID = d.DEPARTMENT_ID
---                  group by DEPARTMENT_NAME)
--- order by DEPARTMENT_ID asc;
+order by DEPARTMENT_ID;
+
+--находим самые высокие зарплаты в отделах и кто их получает
+select distinct
+  DEPARTMENT_ID,
+  FIRST_NAME,
+  LAST_NAME,
+  max(SALARY)
+  over (
+    partition by DEPARTMENT_ID ) MAXIMALNIE_SALARY
+from DENKLE.EMPLOYEES
+order by DEPARTMENT_ID;
+
+select max(SALARY)
+from DENKLE.EMPLOYEES
+group by DEPARTMENT_ID;
+
+select
+  EMPLOYEES.DEPARTMENT_ID,
+  max(SALARY)
+from DENKLE.EMPLOYEES
+  join DENKLE.DEPARTMENTS on EMPLOYEES.DEPARTMENT_ID = DEPARTMENTS.DEPARTMENT_ID
+group by EMPLOYEES.DEPARTMENT_ID;
+
+select e1.*
+from DENKLE.EMPLOYEES e1
+where e1.SALARY = (select max(e2.SALARY)
+                   from DENKLE.EMPLOYEES e2
+                   where e1.DEPARTMENT_ID = e2.DEPARTMENT_ID);
+
+--находим сотрудников со второй самой высокой зарплатой с конца
+select *
+from DENKLE.EMPLOYEES e1
+where SALARY = (select max(SALARY)
+                from DENKLE.EMPLOYEES e2
+                where e2.SALARY < (select max(e3.SALARY)
+                                   from DENKLE.EMPLOYEES e3
+                                   where e2.DEPARTMENT_ID = e3.DEPARTMENT_ID) and e1.DEPARTMENT_ID = e2.DEPARTMENT_ID);
+
+-- находим сотрудников, которые получают больше своих начальников
+select *
+from DENKLE.EMPLOYEES e1, DENKLE.EMPLOYEES e2
+where e1.SALARY > e2.SALARY and e1.MANAGER_ID = e2.EMPLOYEE_ID;
+
+--Вывести список ID отделов, количество сотрудников в которых не превышает 3 человек
+select DEPARTMENT_ID
+from DENKLE.EMPLOYEES
+group by DEPARTMENT_ID
+having count(*) < 3;
+
+--Вывести список сотрудников, не имеющих назначенного руководителя, работающего в том-же отделе
+select * -- выбираем список
+from DENKLE.EMPLOYEES e1e -- из таблицы эмплои
+  left join DENKLE.EMPLOYEES e2m  -- объединяем с собой же
+    on e2m.EMPLOYEE_ID =
+       e1e.MANAGER_ID -- где id мэнеджера сотрудника таблицы сотрудников = ID сотрудника из таблицы менеджеров
+where e2m.DEPARTMENT_ID <> e1e.DEPARTMENT_ID; -- там где ID отдела менеджера не равен ID отдела его подчинённого
+
+-- Найти список ID отделов с максимальной суммарной зарплатой сотрудников
+with sum_salary as
+( select department_id, sum(salary) salary
+  from   DENKLE.EMPLOYEES
+  group  by department_id )
+select department_id
+from   sum_salary a
+where  a.salary = ( select max(salary) from sum_salary );
